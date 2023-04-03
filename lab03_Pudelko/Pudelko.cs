7523ffbda00f6ae9a5bceba4d1220e1d0a744e85
Małgorzata Chudy <malgorzata.chudy@microsoft.wsei.edu.pl>
2023-04-03 23:06:40 +0200

    Add project files.

---
+++ lab03_Pudelko/Pudelko.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PudelkoLibrary
+{
+    public class Pudelko
+    {
+        public double A { get; init; }
+        public double B { get; init; }
+        public double C { get; init; }
+
+        public Pudelko(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public Pudelko() : this(10, 10, 10) { }
+    }
+}
