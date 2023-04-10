9c040c5c2802360d5e87f7bda284971c6b1a0460
Małgorzata Chudy <malgorzata.chudy@microsoft.wsei.edu.pl>
2023-04-11 00:02:08 +0200

    145 tests passed

--- lab03_Pudelko/Program.cs
+++ lab03_Pudelko/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PudelkoLibrary;
+
+Pudelko p = new Pudelko();
+
+Console.WriteLine(p.ToString());
+
+Pudelko pudelko = new Pudelko(9, default,default , PudelkoLibrary.Enums.UnitOfMeasure.centimeter);
+
+Console.WriteLine(pudelko.ToString());
--- lab03_Pudelko/Pudelko.cs
+++ lab03_Pudelko/Pudelko.cs
@@ -10,28 +10,28 @@ namespace PudelkoLibrary
 
     public class Pudelko
     {
+
         public double a;
         public double b;
         public double c;
         public UnitOfMeasure Unit { get; init; }
         public double A
         {
-            get { return Math.Round(ConvertToMeters(a, Unit), 3); }
+            get => ConvertToMeters(a, Unit);
             set { a = value; }
         }
 
         // public double a { get; init; }
         public double B
         {
-            get { return Math.Round(ConvertToMeters(b, Unit), 3); }
+            get { return ConvertToMeters(b, Unit); }
             set { a = value; }
         }
         public double C
         {
-            get { return Math.Round(ConvertToMeters(c, Unit), 3); }
+            get { return ConvertToMeters(c, Unit); }
             set { a = value; }
         }
-
 
 
         public Pudelko(double a = 0.1, double b = 0.1, double c = 0.1, UnitOfMeasure unit = UnitOfMeasure.meter)
@@ -39,7 +39,7 @@ namespace PudelkoLibrary
             this.a = a;
             this.b = b;
             this.c = c;
-            this.Unit = unit;
+            Unit = unit;
 
             //conversion
             double aMeters = ConvertToMeters(a, Unit);
@@ -47,57 +47,27 @@ namespace PudelkoLibrary
             double cMeters = ConvertToMeters(c, Unit);
 
 
-            if ((a <= 0) || (b <= 0) || (c <= 0))
+            if ((aMeters <= 0) || (bMeters <= 0) || (c <= 0))
             {
                 throw new ArgumentOutOfRangeException("The sizes of the box can not have negative values!");
             }
-            else if ((aMeters > 10) || (bMeters > 10) || (cMeters > 10))
+            else if ((aMeters >= 10) || (bMeters >= 10) || (cMeters >= 10))
             {
                 throw new ArgumentOutOfRangeException("The size of box can not be greater than 10 meters!");
             }
-
-            switch (unit)
-            {
-                case UnitOfMeasure.meter:
-                    {
-                        if ((a > 10) || (b > 10) || (c > 10))
-                        {
-                            
-                        }
-                        break;
-                    }
-                case UnitOfMeasure.centimeter:
-                    {
-                        if ((a > 100) || (b > 100) || (c > 10))
-                        {
-                            throw new ArgumentOutOfRangeException("The size of box can not be greater than 10 meters!");
-                        }
-                        break;
-
-                    }
-                case UnitOfMeasure.milimeter:
-                    {
-                        if ((a > 1000) || (b > 1000) || (c > 1000))
-                        {
-                            throw new ArgumentOutOfRangeException("The size of box can not be greater than 10 meters!");
-                        }
-                        break;
-
-                    }
-            } 
         }
 
 
         private double ConvertToMeters(double value, UnitOfMeasure unit)
         {
-            switch(unit)
+            switch (unit)
             {
                 case UnitOfMeasure.meter:
-                    return value;
+                    return Math.Round(value, 3);
                 case UnitOfMeasure.centimeter:
-                    return Math.Round(value / 100, 1);
+                    return Math.Truncate(value * 100.0) / 100.0;
                 case UnitOfMeasure.milimeter:
-                    return Math.Round(value / 1000, 0);
+                    return Math.Truncate(value * 1000.0) / 1000.0;
                 default:
                     throw new ArgumentException("Please provide correct unit of measure");
             }
